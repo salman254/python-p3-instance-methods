44d84ac9f6c10bb2214837b54152cca9c7b70b1a
Salman <salmaanmohamed700@gmail.com>
2025-05-10 14:54:59 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-class Dog:
-    # Class body goes here
+# lib/dog.py
 
-    #Instance method definition
-    pass
+class Dog:
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
+
+# Testing the methods
+if __name__ == "__main__":
+    fido = Dog()
+    fido.bark()  # Should print "Woof!"
+    fido.sit()   # Should print "The dog is sitting."
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
+# lib/person.py
 
-    #Instance method definition
-    pass
+class Person:
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
+
+# Testing the methods
+if __name__ == "__main__":
+    john = Person()
+    john.talk()  # Should print "Hello World!"
+    john.walk()  # Should print "The person is walking."
